22d1540128c6c370aed04c266d5d9cd71304d62a
andycwk <andy@sharpbytes.net>
2012-08-30 16:25:54 +0100

    Added stuff

---
+++ src/app/SharpBytes.PersonalBlog/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpBytes.PersonalBlog.Modules
+{
+    using Nancy;
+
+    public class HomeModule: NancyModule
+    {
+        public HomeModule()
+        {
+            Get[ "/" ] = parameters => "Hello Nancy World";
+        }
+    }
+}
